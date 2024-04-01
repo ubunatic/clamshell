0be3e68020ec94442ac0a48a8bfb1c3c31ec68a0
Uwe Jugel <ubunatic@noreply.codeberg.org>
2024-04-02 00:37:06 +0200

    update Formula sha256 for v1.0.0

--- Formula/clamshell.rb
+++ Formula/clamshell.rb
@@ -2,7 +2,7 @@ class Clamshell < Formula
     desc "CLI for safely managing MacBook clamshell mode."
     homepage "https://github.com/ubunatic/clamshell"
     url "https://github.com/ubunatic/clamshell/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 ""
+    sha256 "323877459f1c1047d3437f73215e813cfe52ce5e0314671ef81429fa585c2e71"
     head "https://github.com/ubunatic/clamshell.git", branch: "main"
 
     def install
