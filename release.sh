2eaa0f7ed831769fbf505f711081af3c1053c0d2
Uwe Jugel <ubunatic@noreply.codeberg.org>
2024-04-01 13:18:35 +0200

    release title

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 VERSION=$1
+TITLE="${2:-"Clamshell $VERSION"}"
 
 set -o errexit
 
@@ -14,29 +15,34 @@ fi
 
 if git diff --exit-code
 then echo "Working directory is clean"
+elif read -rp "Stage all changes? (y/N) " yesno && test "$yesno" = "y" && git add .
+then echo "Staged pending changes"
 else echo "Please commit changes before running this script!"; exit 1
 fi
 
 if grep -q "$VERSION" Formula/clamshell.rb
-then echo "Found version $VERSION in Formula/clamshell.rb"
-else echo "Version $VERSION not found in Formula/clamshell.rb"
+then echo "Version $VERSION found in Formula/clamshell.rb"
+else echo "Setting $VERSION in Formula/clamshell.rb"
      sed -i '' -e "s/tag: \"v.*\"/tag: \"$VERSION\"/g" Formula/clamshell.rb
      git add Formula/clamshell.rb
      git commit -m "set version to $VERSION"
      git tag "$VERSION" -f
 fi
 
+git push -f
 git push --tags -f
-echo "Version tag $VERSION committed and pushed"
+echo "Version $VERSION code and tag committed and pushed"
 
 if ! type gh > /dev/null
 then echo "Github CLI not found, release incomplete"; exit 1
 fi
 
 if gh release view "$VERSION" > /dev/null
-then echo "Release $VERSION already exists, updating tag"
-     gh release edit "$VERSION" --title "Clamshell $VERSION" --notes "Release $VERSION" --tag "$VERSION"
-else
-     echo "Creating release $VERSION"
-     gh release create "$VERSION" --title "Clamshell $VERSION" --notes "Release $VERSION"
+then echo "Release $VERSION already exists, deleting old release"
+     # also delete all files attached to the release
+     gh release view "$VERSION" --json assets --jq '.assets[].id' | xargs -I {}
+     # gh release delete -y "$VERSION"
 fi
+
+echo "Creating release $VERSION"
+gh release create "$VERSION" --title "$TITLE" --notes "Release $VERSION"
