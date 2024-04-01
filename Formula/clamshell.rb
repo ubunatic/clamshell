a40ee47e14fd06f988a7fe1daaa920ec03a6a997
Uwe Jugel <ubunatic@noreply.codeberg.org>
2024-04-02 00:57:40 +0200

    update Formula sha256 for v1.0.0

--- Formula/clamshell.rb
+++ Formula/clamshell.rb
@@ -2,7 +2,7 @@ class Clamshell < Formula
     desc "Clamshell manages your closed MacBook's sleep when an external display is attached."
     homepage "https://github.com/ubunatic/clamshell"
     url "https://github.com/ubunatic/clamshell/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 ""
+    sha256 "7558a1eaadcb2cddaf815d6a4e41ea1ce22d4f00e7ae798e65e68f632ebd35ce"
     head "https://github.com/ubunatic/clamshell.git", branch: "main"
     depends_on :macos
 
