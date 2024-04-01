a833648d8991e5e8718142eb9809e48f9a121425
Uwe Jugel <ubunatic@noreply.codeberg.org>
2024-04-01 17:21:00 +0200

    rework release scripts

--- Formula/clamshell.rb
+++ Formula/clamshell.rb
@@ -1,7 +1,7 @@
 class Clamshell < Formula
     desc "CLI for safely managing MacBook clamshell mode."
     homepage "https://github.com/ubunatic/clamshell"
-    url "https://github.com/ubunatic/clamshell.git", using: :git, tag: "v1.0.0"
+    url "https://github.com/ubunatic/clamshell/archive/refs/tags/v1.0.0.tar.gz"
     head "https://github.com/ubunatic/clamshell.git", branch: "main"
 
     def install
--- release.sh
+++ release.sh
@@ -13,36 +13,44 @@ else echo "Usage: $0 VERSION"
      exit 1
 fi
 
-if git diff --exit-code
-then echo "Working directory is clean"
-elif read -rp "Stage all changes? (y/N) " yesno && test "$yesno" = "y" && git add .
-then echo "Staged pending changes"
-else echo "Please commit changes before running this script!"; exit 1
-fi
+# if git diff --exit-code
+# then echo "Working directory is clean"
+# elif read -rp "Stage all changes? (y/N) " yesno && test "$yesno" = "y" && git add .
+# then echo "Staged pending changes"
+# else echo "Please commit changes before running this script!"; exit 1
+# fi
 
-if grep -q "$VERSION" Formula/clamshell.rb
-then echo "Version $VERSION found in Formula/clamshell.rb"
-else echo "Setting $VERSION in Formula/clamshell.rb"
-     sed -i '' -e "s/tag: \"v.*\"/tag: \"$VERSION\"/g" Formula/clamshell.rb
-     git add Formula/clamshell.rb
-     git commit -m "set version to $VERSION"
-     git tag "$VERSION" -f
-fi
+# if grep -q "$VERSION" Formula/clamshell.rb
+# then echo "Version $VERSION found in Formula/clamshell.rb"
+# else echo "Setting $VERSION in Formula/clamshell.rb"
+#      sed -i '' -e "s/tag: \"v.*\"/tag: \"$VERSION\"/g" Formula/clamshell.rb
+#      git add Formula/clamshell.rb
+#      git commit -m "set version to $VERSION"
+#      git tag "$VERSION" -f
+# fi
 
-git push -f
-git push --tags -f
-echo "Version $VERSION code and tag committed and pushed"
+# git push -f
+# git push --tags -f
+# echo "Version $VERSION code and tag committed and pushed"
 
-if ! type gh > /dev/null
-then echo "Github CLI not found, release incomplete"; exit 1
-fi
+# download the latest release tarball
+tmp="$(mktemp -d -t clamshell-release-XXXXXX)/$VERSION.tar.gz"
+curl -L "https://github.com/ubunatic/clamshell/archive/refs/tags/$VERSION.tar.gz" -o "$tmp"
+echo "Downloaded release tarball to $tmp"
+echo "computing sha256 checksum"
+shasum -a 256 "$tmp"
+exit 0
 
-if gh release view "$VERSION" > /dev/null
-then echo "Release $VERSION already exists, deleting old release"
-     # also delete all files attached to the release
-     gh release view "$VERSION" --json assets --jq '.assets[].id' | xargs -I {}
-     # gh release delete -y "$VERSION"
-fi
+# if ! type gh > /dev/null
+# then echo "Github CLI not found, release incomplete"; exit 1
+# fi
 
-echo "Creating release $VERSION"
-gh release create "$VERSION" --title "$TITLE" --notes "Release $VERSION"
+# if gh release view "$VERSION" > /dev/null
+# then echo "Release $VERSION already exists, deleting old release"
+#      # also delete all files attached to the release
+#      gh release view "$VERSION" --json assets --jq '.assets[].id' | xargs -I {}
+#      # gh release delete -y "$VERSION"
+# fi
+
+# echo "Creating release $VERSION"
+# gh release create "$VERSION" --title "$TITLE" --notes "Release $VERSION"
